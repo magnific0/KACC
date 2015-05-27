7f88410d78c5fdb5ec0b193b2c0cce6e642108da
Jacco Geul <jacco@geul.net>
2015-05-27 16:44:16 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# KACC - Kiruna All-sky Camera Cloudiness
+
+## Description
+The script uses ImageMagick for identifying clouds in pictures of the Kiruna All-sky
+camera. The script tries to differentiate between clouds and
+aurora in images and computes a cloudiness fraction which is tested
+against a threshold value.
+
+Inspired by Patrik Karrang.
+
+## Requirements
+* ImageMagick (tested with ImageMagick 6.9.1-2 Q16 x86_64)
+
+## Usage
+1. Download a number of images ready for processing.
+2. Place the script into the same folder.
+3. From the terminal execute ```./bacc.sh``` (make sure the script is executable).
+4. The script will output which images are cloudy and which are not and report the cloudiness factor.
+
+## Settings
+You can change parameters such as the thresold in the script itself.
+
+### Threshold
+Thresold before image is considered too cloudy. Keep in mind that since this is a circle in a rectangular black image, therefore a good portion of the image remains black even when fully clouded. Fraction values range from 0.00 to 0.30ish (default 0.0004).
+
+### HSV range settings for clouds and aurora
+Based the HSV settings clouds and aurora can be differentiated. These values can be obtained by manually inspecting an image the color picker in any popular image procesing program (e.g., GIMP). Make sure to get the HSL values (not RGB, HSV, HSB etc).
+
+| Name         | Range   | Aurora   | Clouds   |
+| ------------ | ------- | -------- | -------- |
+| (H)ue        | [0,360] | [80,95]  | [20,40]  |
+| (S)aturation | [0,100] | [30,70]  | [50,95]  |
+| (L)ightness  | [0,100] | [10,100] | [10,100] |
+
+It is adviced to use broad ranges and tweak accordingly by inspecting the intermediate results (```result1.png```/```result2.png```).
+
+### File pattern
+All files in the same folder as the script that match ```*.JPG``` (see ```for f in *.JPG``` statement) will be processed by the script. Adapt this expression to your needs.
+
+## References
+* http://www.irf.se/allsky/rtascirf.php
+* http://www.irf.se/allsky/data.html
+* http://www.wizards-toolkit.org/discourse-server/viewtopic.php?f=1&t=25660
+* http://www.imagemagick.org/Usage/
---
+++ kacc.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# KACC - Kiruna All-sky Camera Cloudiness, version 1
+# Jacco Geul <jacco@geul.net>, 2015
+#
+# The script uses imagemagick for identifying clouds in Kiruna All-sky
+# camera images. The script tries to differentiate between clouds and
+# aurora in images and computes a cloudiness fraction which is tested
+# against a threshold value.
+#
+# Inspired by Patrik Karrang.
+#
+# References
+# - http://www.irf.se/allsky/rtascirf.php
+# - http://www.irf.se/allsky/data.html
+# - http://www.wizards-toolkit.org/discourse-server/viewtopic.php?f=1&t=25660
+# - http://www.imagemagick.org/Usage/
+
+# Threshold
+# Keep in mind that since this is a circle in a black image a good
+# portion of the image remains black even when fully clouded. Fraction
+# values range from 0.00 to 0.30ish.
+threshold=0.004
+
+# Filter settings for aurora map
+# You obtain these by inspecting an image manually using the color
+# picker in GIMP or PhotoShop. Make sure to get the HSL values (not
+# RGB, HSV, HSB, CMY, etc!!!).
+#
+# Use broad ranges and tweak accordingly by inspecting the
+# intermediate result (result1.png).
+#
+# (H)ue        [0,360]
+# (S)aturation [0,100]
+# (L)ightness  [0,100]
+auror_Hmin=80
+auror_Hmax=95
+auror_Smin=30
+auror_Smax=70
+auror_Lmin=10
+auror_Lmax=100
+
+# Create filter
+convert -size 1x360 gradient: -fx "(u>$auror_Hmin/360 && u<$auror_Hmax/360)?white:black" auror_h.png
+convert -size 1x360 gradient: -fx "(u>$auror_Smin/100 && u<$auror_Smax/100)?white:black" auror_s.png
+convert -size 1x360 gradient: -fx "(u>$auror_Lmin/100 && u<$auror_Lmax/100)?white:black" auror_l.png
+convert auror_h.png auror_s.png auror_l.png -combine auror_lut.png
+convert auror_lut.png -flip auror_lut.png
+
+# Filter settings for cloud map (as before)
+cloud_Hmin=20
+cloud_Hmax=40
+cloud_Smin=50
+cloud_Smax=95
+cloud_Lmin=10
+cloud_Lmax=100
+
+# Create filter
+convert -size 1x360 gradient: -fx "(u>$cloud_Hmin/360 && u<$cloud_Hmax/360)?white:black" cloud_h.png
+convert -size 1x360 gradient: -fx "(u>$cloud_Smin/100 && u<$cloud_Smax/100)?white:black" cloud_s.png
+convert -size 1x360 gradient: -fx "(u>$cloud_Lmin/100 && u<$cloud_Lmax/100)?white:black" cloud_l.png
+convert cloud_h.png cloud_s.png cloud_l.png -combine cloud_lut.png
+convert cloud_lut.png -flip cloud_lut.png
+
+# Loop over folder
+for f in *.JPG
+do
+    convert $f temp0.png
+
+    # Compute filtered images > maps
+    convert \( temp0.png -colorspace HSL \) auror_lut.png -clut -separate -evaluate-sequence multiply temp1.png
+    convert \( temp0.png -colorspace HSL \) cloud_lut.png -clut -separate -evaluate-sequence multiply temp2.png
+    convert -monochrome temp1.png temp1.png
+    convert -monochrome temp2.png temp2.png
+
+    # We want to invert the aurora map and make the white transparent as
+    # it is subtractive
+    convert -negate temp1.png temp1.png
+    convert temp1.png -transparent white temp1.png
+
+    # Now create a composite map
+    composite -compose Bumpmap temp1.png temp2.png -alpha Set temp3.png
+
+    # Count white percentage
+    fraction=$(convert temp3.png -format "%[fx:mean]" info:)
+
+    # Do a test and do something based on outcome.
+    if [ 1 -eq `echo "${fraction} < ${threshold}" | bc` ]; then
+        echo "$f is NOT cloudy ($fraction/$threshold)"
+    else
+        echo "$f is too cloudy ($fraction/$threshold)"
+    fi
+done
